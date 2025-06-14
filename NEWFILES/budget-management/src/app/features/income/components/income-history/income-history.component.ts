afd20272bee0e6b6d671240a47c968ecbc59635f
Saleem Yousef <130926254+Arikatakur@users.noreply.github.com>
2025-06-14 04:38:23 +0300

    change income-history to income

--- NEWFILES/budget-management/src/app/features/income/components/income-history/income-history.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-income-history',
-  standalone: true,
-  templateUrl: './income-history.component.html',
-  styleUrl: './income-history.component.css'
-})
-export class IncomeHistoryComponent {}
