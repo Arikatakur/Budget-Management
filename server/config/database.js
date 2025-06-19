5d88220e75f12623e1927a22f2835489385ed7df
Saleem Yousef <130926254+Arikatakur@users.noreply.github.com>
2025-06-19 10:41:36 +0300

    change db-password

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('budget', 'root', '12345', {
+const sequelize = new Sequelize('budget', 'root', 'admin', {
   host: 'localhost',
   dialect: 'mysql'
 });
