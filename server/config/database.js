5eb2ccfd2a4c3a6a3c3e31fbfac65165ecd57d06
MontherIB-SWE <mndr6702@gmail.com>
2025-07-24 23:15:40 +0300

    better Structure

--- server/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('budget', 'root', '12345', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-module.exports = sequelize;
